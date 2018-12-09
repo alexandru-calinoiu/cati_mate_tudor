a0782cf89e64c28389145f1e05c95bcb1293e907
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2018-12-09 11:41:31 +0200

    Tema la matematica

---
+++ adunare.rb
@@ -0,0 +1,10 @@
+def aduna(numaru1, numaru2)
+  numaru1 + numaru2
+end
+
+p aduna(9, 0)
+p aduna(1, 7)
+p aduna(2, 5)
+p "3 + 2 = #{aduna(3, 2)}"
+p "9 + 1 = #{aduna(9, 1)}"
+p "7 + 1 = #{aduna(7, 1)}"
